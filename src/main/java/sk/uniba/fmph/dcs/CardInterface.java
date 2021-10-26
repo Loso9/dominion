58dd03230abe3250c632ae7eb994148bf03f153d
loson <losonsky551@gmail.com>
2021-10-26 21:06:43 +0200

    fixing mistakes in CardInterface

--- src/main/java/sk/uniba/fmph/dcs/CardInterface.java
+++ src/main/java/sk/uniba/fmph/dcs/CardInterface.java
@@ -1,8 +1,8 @@
 package sk.uniba.fmph.dcs;
 
 interface CardInterface {
-    public void evaluate(TurnStatus ts);
-    public GameCardType cardType();
+    void evaluate(TurnStatus ts);
+    GameCardType cardType();
 }
 
 
