dd438d48a2a0a690a81e26a909d45d9aed329e30
loson <losonsky551@gmail.com>
2021-10-26 21:00:33 +0200

    refactoring existing class TurnStatus to java-ideals

--- src/main/java/sk/uniba/fmph/dcs/TurnStatus.java
+++ src/main/java/sk/uniba/fmph/dcs/TurnStatus.java
@@ -1,9 +1,16 @@
 package sk.uniba.fmph.dcs;
 
 public class TurnStatus {
-    public int actions;
-    public int buys;
-    public int coins;
+    private int actions;
+    private int buys;
+    private int coins;
+
+    public TurnStatus() {
+        this.actions = 0;
+        this.buys = 0;
+        this.coins = 0;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -26,8 +33,30 @@ public class TurnStatus {
             return false;
         if (buys != other.buys)
             return false;
-        if (coins != other.coins)
-            return false;
-        return true;
+        return coins == other.coins;
+    }
+
+    public int getActions() {
+        return actions;
+    }
+
+    public void addActions(int actions) {
+        this.actions += actions;
+    }
+
+    public int getBuys() {
+        return buys;
+    }
+
+    public void addBuys(int buys) {
+        this.buys += buys;
+    }
+
+    public int getCoins() {
+        return coins;
+    }
+
+    public void addCoins(int coins) {
+        this.coins += coins;
     }
 }
