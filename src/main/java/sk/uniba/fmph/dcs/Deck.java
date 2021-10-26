5ce30f0558416e882ef3a61d864e338c6562a43f
loson <losonsky551@gmail.com>
2021-10-26 11:16:34 +0200

    implementing Deck class

--- src/main/java/sk/uniba/fmph/dcs/Deck.java
+++ src/main/java/sk/uniba/fmph/dcs/Deck.java
@@ -1,4 +1,42 @@
 package sk.uniba.fmph.dcs;
 
+import java.util.*;
+
 public class Deck {
+
+    private LinkedList<CardInterface> deckOfCards;
+
+    public Deck() {
+        deckOfCards = new LinkedList<>();
+        for (int i = 0; i < 3; i++) {
+            deckOfCards.add(new GameCard(GameCardType.GAME_CARD_TYPE_ESTATE));
+        }
+
+        for (int j = 0; j < 7; j++) {
+            deckOfCards.add(new GameCard(GameCardType.GAME_CARD_TYPE_COPPER));
+        }
+        shuffle();
+    }
+
+    public Deck(List<CardInterface> deckOfCards) {
+        this.deckOfCards = new LinkedList<>(deckOfCards);
+    }
+
+    public List<CardInterface> draw(Integer count) {
+        int cardCount = count;
+        ArrayList<CardInterface> returnList = new ArrayList<>();
+        while (cardCount --> 0) {
+            returnList.add(deckOfCards.removeFirst());
+        }
+
+        return Collections.unmodifiableList(returnList);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(deckOfCards);
+    }
+
+    public LinkedList<CardInterface> getDeckOfCards() {
+        return deckOfCards;
+    }
 }
--- src/main/java/sk/uniba/fmph/dcs/GameCard.java
+++ src/main/java/sk/uniba/fmph/dcs/GameCard.java
@@ -1,4 +1,27 @@
 package sk.uniba.fmph.dcs;
 
-public class GameCard {
+public class GameCard implements CardInterface {
+    private GameCardType gameCardType;
+
+    public GameCard() {
+        this.gameCardType = null;
+    }
+
+    public GameCard(GameCardType gameCardType) {
+        this.gameCardType = gameCardType;
+    }
+
+    public void setGameCard(GameCardType gameCardType) {
+        this.gameCardType = gameCardType;
+    }
+
+    @Override
+    public void evaluate(TurnStatus ts) {
+
+    }
+
+    @Override
+    public GameCardType cardType() {
+        return gameCardType;
+    }
 }
