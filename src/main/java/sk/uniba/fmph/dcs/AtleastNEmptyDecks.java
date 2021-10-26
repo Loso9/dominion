06c8d468bede0ae20b1f72a93c4145864a6cabc5
loson <losonsky551@gmail.com>
2021-10-26 20:59:24 +0200

    implementing AtleastNEmptyDecks class for gameover condition

--- src/main/java/sk/uniba/fmph/dcs/AtleastNEmptyDecks.java
+++ src/main/java/sk/uniba/fmph/dcs/AtleastNEmptyDecks.java
@@ -1,4 +1,24 @@
 package sk.uniba.fmph.dcs;
 
-public class AtleastNEmptyDecks {
+import java.util.*;
+
+public class AtleastNEmptyDecks implements EndGameStrategy {
+
+    ArrayList<BuyDeck> supply;
+
+    public AtleastNEmptyDecks(List<BuyDeck> supply) {
+        this.supply = new ArrayList<>(supply);
+    }
+
+    //ak su 3 buydecky prazdne
+    @Override
+    public boolean isGameOver() {
+        int count = 0;
+        for (BuyDeck deck : supply) {
+            if (deck.isEmpty()) {
+                count++;
+            }
+        }
+        return count >= 3;
+    }
 }
