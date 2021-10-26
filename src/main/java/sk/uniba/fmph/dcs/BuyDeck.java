3027cda00ad9bddf9e0ee1efc8ff7d9625850f42
loson <losonsky551@gmail.com>
2021-10-26 22:49:56 +0200

    basic implementation of BuyDeck class

--- src/main/java/sk/uniba/fmph/dcs/BuyDeck.java
+++ src/main/java/sk/uniba/fmph/dcs/BuyDeck.java
@@ -1,4 +1,42 @@
 package sk.uniba.fmph.dcs;
 
+import java.util.*;
+
 public class BuyDeck {
+
+    private final ArrayList<CardInterface> supply;
+    private int countCard;
+    private final GameCardType gameCardType;
+    private final TurnStatus ts;
+
+    public BuyDeck(TurnStatus ts, GameCardType gameCardType, int countCard) {
+        this.ts = ts;
+        this.gameCardType = gameCardType;
+        this.supply = new ArrayList<>();
+        this.countCard = countCard;
+        for (int i = 0; i < countCard; i++) {
+            supply.add(new GameCard(gameCardType));
+        }
+    }
+
+    public Optional<CardInterface> buy() {
+        CardInterface card = null;
+        if (ts.getBuys() > 0) {
+            card = supply.remove(0);
+            countCard--;
+        }
+        return Optional.ofNullable(card);
+    }
+
+    public int getCountCard() {
+        return countCard;
+    }
+
+    public boolean isEmpty() {
+        return getCountCard() == 0;
+    }
+
+    public GameCardType getGameCardType() {
+        return gameCardType;
+    }
 }
