3aac4dcdb6b90ed9ace0af7e0432b7fbdb8e24a8
loson <losonsky551@gmail.com>
2021-10-29 16:41:09 +0200

    reworking AtleastNEmptyDecks to check both n-empty decks and empty province card deck

--- src/main/java/sk/uniba/fmph/dcs/AtleastNEmptyDecks.java
+++ src/main/java/sk/uniba/fmph/dcs/AtleastNEmptyDecks.java
@@ -14,6 +14,10 @@ public class AtleastNEmptyDecks implements EndGameStrategy {
 
     @Override
     public boolean isGameOver() {
+        return nEmptyDecks() || emptyProvinceCardDeck();
+    }
+
+    public boolean nEmptyDecks() {
         int count = 0;
         for (BuyDeck deck : supply) {
             if (deck.isEmpty()) {
@@ -21,5 +25,21 @@ public class AtleastNEmptyDecks implements EndGameStrategy {
             }
         }
         return count >= numOfEmptyDecks;
+    }
+
+    public boolean emptyProvinceCardDeck() {
+        BuyDeck provinceBuyDeck = findBuyDeck(GameCardType.GAME_CARD_TYPE_PROVINCE);
+        return provinceBuyDeck.isEmpty();
+    }
+
+    //cant return null if supply is well-constructed
+    public BuyDeck findBuyDeck(GameCardType cardType) {
+        BuyDeck buyDeckToFind = null;
+        for (BuyDeck buyDeck : supply) {
+            if (buyDeck.getGameCardType().equals(cardType)) {
+                buyDeckToFind = buyDeck;
+            }
+        }
+        return buyDeckToFind;
     }
 }
